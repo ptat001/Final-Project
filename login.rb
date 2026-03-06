1dd18ce8640d8183f515b24a0b0a3faed0971c5f
prateek <prateekrajput539@gmail.com>
2026-03-06 20:25:16 +0530

    Add New Fetures

--- login.rb
+++ login.rb
@@ -1,55 +1,110 @@
 require 'csv'
 
-module LoginLogic
+module Loginlogic
   def self.login_user 
     def self.validate(ep, pe) 
-      #return false unless File.exist?("test.csv")
+      
       a = File.readlines("test.csv") 
       a.any? do |line| 
-        e, p = line.chomp.split(",") 
+        n, e, c, p = line.chomp.split(",") 
         e == ep && p == pe
       end 
     end  
-
-    def self.create_task 
-      print "Enter Title: " 
-      @title = gets.chomp 
-      print "Description: " 
-      @desc = gets.chomp 
-      print "Enter Status pending/complited: " 
-      @status = gets.chomp  
-      print "Created By: " 
-      @user = gets.chomp
-
-      CSV.open("task.csv", "a+") do |csv| 
-        csv << [@title, @desc, @status, @user] 
-      end  
-      puts "Task Created !!! "
-    end 
-
-    def self.view_all_task 
-      puts "-----All Tasks-----" 
+    
+    def self.create_task   
       
-        a = File.readlines("task.csv") 
-        puts a 
+      puts "--- Create New Task ---"
+      
+      attempts = 0 
+      loop do
+        if attempts >= 3
+          puts "Maximum title attempts reached. Returning to menu..."
+          return 
+        end
+        
+        print "Enter Title: " 
+        @title = gets.strip   
+        
+        if task_validate(@title) 
+          puts "Error: Title Already Exists"  
+          attempts += 1 
+        elsif @title =~ /\A[a-zA-Z ]+\z/ && !@title.empty?
+          break 
+        else 
+          puts "Error: Title must contain only alphabets/spaces" 
+          attempts += 1
+        end
+      end 
+      
+      
+      
+      attempts = 0
+      loop do
+        return if attempts >= 3
+        print "Enter Description: " 
+        @desc = gets.strip 
+        if @desc =~ /\A[a-zA-Z ]+\z/ && !@desc.empty?
+          break 
+        else 
+          puts "Error: Description must contain only alphabets (Attempt #{attempts + 1}/3)"
+          attempts += 1
+        end
+      end
+      
+      
+      attempts = 0
+      loop do
+        return if attempts >= 3
+        print "Enter Status (pending/completed): " 
+        @status = gets.strip.downcase
+        if ["pending", "completed"].include?(@status)
+          break 
+        else
+          puts "Error: Status must be 'pending' or 'completed'!"
+          attempts += 1
+        end
+      end 
+      
+      @date = Time.now.strftime("%d/%m/%Y")
+      @user = @email
+      
+      CSV.open("task.csv", "a+") do |csv| 
+        csv << [@title, @desc, @status, @user, @date] 
+      end  
+      puts "Task Created Successfully !!!"
+      
+    end 
+    
+    def self.view_all_task(e2)
+      puts "-----All Tasks-----" 
+      File.readlines("task.csv").each do |line|  
+        t, d, s, n, d3= line.chomp.split(",") 
+        if (n == e2) 
+          puts "#{t},#{d},#{s},#{n},#{d3}" 
+        end
+        
+      end  
       
     end
     
     def self.task_by_Specific_user
-      puts "Enter the name" 
-      name = gets.chomp
-      puts "-----Tasks Created By #{name} Are-------" 
-      
+      puts "Please enter the email" 
+      email_1 = gets.chomp 
+      if email_1=~ /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+        puts "-----Tasks Created By #{email_1} Are-------" 
+        
         File.readlines("task.csv").each do |line|  
           t, d, s, n = line.chomp.split(",") 
-          if (n == name) 
+          if (n == email_1) 
             puts "Title :#{t},Desc :#{d},Status :#{s}" 
           end 
         end 
-      
+      else 
+        puts"Invalid Email Formate" 
+      end
       puts "No Task Available " 
     end 
-
+    
     def self.delete_task 
       print"Enter the Title of the task to delete: " 
       target_title=gets.chomp 
@@ -65,74 +120,202 @@ module LoginLogic
         puts("!!! ERROR : Task With Title #{target_title} Not Found !!!") 
       end
     end 
-   
-    def self.update_task_Status 
-       puts "Enter the Title of the task you want to update:" 
-       target_title = gets.chomp.strip 
-
     
-       lines = File.readlines("task.csv") 
-       found = false
-
-       updated_lines = lines.map do |line| 
-         t, d, s, n = line.chomp.split(",").map(&:strip)
-
-         if t == target_title 
+    
+    def self.update_task_Status  
+      title_attempts = 0
+      target_title = nil
+      
+      loop do  
+        if title_attempts >= 3
+          puts "Maximum title attempts reached. Returning to menu..."
+          return 
+        end
+        puts"Enter the Title of the task you want to update: " 
+        target_title = gets.strip  
+        if task_validate(target_title) 
+          break 
+        else
+          title_attempts += 1
+          puts "!!! ERROR: Task With Title '#{target_title}' Not Found !!! (Attempt #{title_attempts}/3)"
+        end
+      end 
+      return puts "No tasks found." unless File.exist?("task.csv")
+      lines = File.readlines("task.csv") 
+      
+      found = false
+      already_completed = false
+      
+      updated_lines = lines.map do |line| 
+        t, d, s, u, dt = line.chomp.split(",").map(&:strip)
+        if t == target_title 
           found = true
-          puts "Current Status: #{s}"
-          print "Enter New Status (pending/completed): "
-          new_status = gets.chomp
-          "#{t},#{d},#{new_status},#{n}\n" 
-
-         else 
-           line
-         end 
-       end  
-       if found 
-         File.write("task.csv", updated_lines.join) 
-         puts "Task Updated Successfully!"
+          
+          
+          if s.downcase == "completed"
+            already_completed = true
+            line 
+          else
+            attempts = 0
+            new_status = nil
+            loop do
+              if attempts >= 3
+                puts "Max attempts reached. Task remains 'pending'."
+                new_status = s
+                break
+              end
+              
+              puts "Current Status: #{s}"
+              print "Enter New Status (completed): "
+              input = gets.strip.downcase
+              
+              if input == "completed"
+                new_status = "completed"
+                break
+              else
+                puts "Error: You can only update to 'completed'. (Attempt #{attempts + 1}/3)"
+                attempts += 1
+              end
+            end
+            "#{t},#{d},#{new_status},#{u},#{dt}\n" 
+          end
         else 
-          puts "!!! ERROR: Task With Title '#{target_title}' Not Found !!!" 
+          line 
         end 
-    end 
+      end
+      
+      
+      if already_completed
+        puts "!!! ERROR: Task '#{target_title}' is already completed and cannot be updated."
+      elsif found 
+        File.write("task.csv", updated_lines.join) 
+        puts "Task Updated Successfully!"
+      else 
+        puts "!!! ERROR: Task With Title '#{target_title}' Not Found !!!" 
+      end  
+    end
     
-       
     
-    puts "--- User Login ---"
-    print "Enter email: "
-    @email = gets.strip 
+    puts "--- User Login ---" 
     
+    attempts = 0 
+    
+    loop do
+      
+      if attempts >= 3
+        puts "Too many failed login attempts. Returning to main menu..."
+        break
+      end
+      
+      print "Enter email: "
+      @email = gets.strip 
+      
       print "Enter password: "
       @password = gets.strip  
-
-      if self.validate(@email, @password)   
       
-        loop do
-          puts "\n------- Welcome -------" 
-          puts "1. Create Task"
-          puts "2. View All Task"
-          puts "3. View task by specific user"
-          puts "4. Delete Task" 
-          puts "5. Status Update" 
-          puts "6. Logout" 
-          print "Enter your choice: "
-          choice = gets.strip
-          case choice
-          when "1" then create_task 
-          when "2" then view_all_task
-          when "3" then task_by_Specific_user 
-          when "4" then delete_task 
-          when "5" then update_task_Status
-          when "6"
-           puts "Logging out..."
-          break
-         else
-           puts "Invalid choice." 
-         end
-       end 
-     else 
-      puts "Invalid Email Or Password !!!!!"
-     end 
+      if self.validate(@email, @password)   
+        
+        return task_menue
+      else 
+        
+        attempts += 1
+        puts "Invalid Email Or Password!)"
+        next 
+      end 
+    end 
     
-  end
-end
+    
+    
+    def self.task_validate(h)
+      a = File.readlines("task.csv") 
+      a.any? do |line| 
+        t1,d1,s1,e,d = line.chomp.split(",") 
+        t1 == h 
+      end 
+      
+    end 
+    
+    
+  end 
+  
+  def self.task_menue 
+    loop do
+      puts "\n------- Welcome -------" 
+      puts "1. Create Task"
+      puts "2. View All Task"
+      puts "3. View task by specific user"
+      puts "4. Delete Task" 
+      puts "5. Status Update"  
+      puts "6. view all users"  
+      puts "7. assign task"
+      puts "8. Logout" 
+      print "Enter your choice: "
+      choice = gets.strip
+      case choice
+      when "1" then create_task 
+      when "2" then view_all_task(@email)
+      when "3" then task_by_Specific_user 
+      when "4" then delete_task 
+      when "5" then update_task_Status 
+      when "6" then view_all_users 
+      when "7" then assign_task
+      when "8"
+        puts "Logging out..."
+        return
+      else
+        puts "Invalid choice." 
+      end
+    end 
+    
+  end  
+  
+  def self.view_all_users 
+    puts "-----All Users-----" 
+    
+    a = File.readlines("test.csv") 
+    puts a 
+  end 
+  
+  def self.assign_task 
+    puts "--- Assign Task to Another User ---"
+    
+    print "Enter the Title of the task: "
+    @target_title = gets.strip
+    unless task_validate(@target_title)
+      puts "Error: Task with title '#{@target_title}' not found."
+      return
+    end 
+    attempts = 0
+    loop do
+      return if attempts >= 3
+      print "Enter email: "
+      @email = gets.strip
+      if @email !~ /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+        puts "Invalid Email Format"
+        attempts += 1  
+        next
+      else 
+        break 
+      end
+    end 
+    lines = File.readlines("task.csv") 
+    found = false
+    ul= lines.map do |line| 
+      t, d, s, n ,dt= line.chomp.split(",").map(&:strip)
+      if t == @target_title 
+        found = true
+        @new_assignee = @email
+        "#{t},#{d},#{s},#{@new_assignee},#{dt}\n" 
+      else 
+        line
+      end 
+    end  
+    if found 
+      File.write("task.csv", ul.join) 
+      puts "Task successfully reassigned "
+      
+    else 
+      puts "!!! ERROR: Not Found !!!" 
+    end 
+  end 
+end  
--- registration.rb
+++ registration.rb
@@ -1,38 +1,102 @@
-require 'csv'
-
-module RegistrationLogic
+require 'csv' 
+require_relative 'login'
+module RegistrationLogic 
+  
   def self.register_user
+    puts "--- User Registration ---"
+
     
-    def self.exist(em)
-      
-      a = File.readlines("test.csv") 
-      a.any? do |line| 
-        e, p = line.chomp.split(",") 
-        e == em
-      end 
+    attempts = 0
+    loop do
+      return if attempts >= 3 
+      print "Enter name: "
+      @username = gets.strip 
+      if !exist(@username,@email,@contact)
+       if @username.match?(/\A[a-zA-Z]+\z/)
+        break
+       else
+        puts "Invalid Username. (Attempts: "
+        attempts += 1
+       end 
+     else 
+       puts"User name alredy exist" 
+     end
     end
 
-    puts "--- User Registration ---"
-    print "Enter email: "
-    @email = gets.strip  
-    if @email =~ /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-      if exist(@email) 
-        puts "email Already Exist" 
-      else 
-        print "Enter password: "
-        @password = gets.strip   
-        if @password.match?(/\A[a-zA-Z0-9@#$%^&*!]+\z/)
-          CSV.open("test.csv", "a+") do |csv| 
-           csv << [@email, @password] 
-         end   
-         puts "Registration done please login"
-        else 
-          puts "Error: Invalid Password Format" 
-        end
-       #puts "Registration done please login"
-     end  
-     else 
-       puts"Invalid Email Format"
+    
+    attempts = 0
+    loop do
+      return if attempts >= 3
+      print "Enter email: "
+      @email = gets.strip
+      if @email !~ /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+        puts "Invalid Email Format"
+        attempts += 1
+      elsif exist(@username,@email,@contact,)
+        puts "Email Already Exist"
+        attempts += 1
+      else
+        break
+      end
     end
+
+    
+    attempts = 0
+    loop do
+      return if attempts >= 3
+      print "Enter contact no (10 digits): "
+      @contact = gets.strip 
+      if !exist(@username,@email,@contact)
+       if @contact.match?(/\A\d{10}\z/)
+        break
+       else
+        puts "Invalid Contact. Must be 10 digits, no spaces/special chars."
+        attempts += 1
+       end 
+      else 
+        puts"Contact no. alredy exist" 
+      end
+    end
+
+    
+    attempts = 0
+    loop do
+      if attempts >= 3
+        puts "Too many password attempts. Restarting registration..."
+        return register_user 
+      end
+      print "Enter 4-digit password: "
+      @password = gets.strip
+      if @password.match?(/\A[0-9]+\z/)
+       
+        CSV.open("test.csv", "a+") do |csv|
+          csv << [@username, @email, @contact, @password]
+        end
+        puts "Registration done."
+        Loginlogic.task_menue
+        return
+      else
+        puts "Error: Invalid Password Use only 4-digit"
+        attempts += 1
+      end
+    end
+
   end
+
+
+  def self.exist(u,em,con)
+     a = File.readlines("test.csv") 
+     a.any? do |line| 
+        n, e, c, p = line.chomp.split(",") 
+        e == em || c == con || n==u
+     end
+   end     
+
+   
+
 end
+ 
+
+
+
+ 
